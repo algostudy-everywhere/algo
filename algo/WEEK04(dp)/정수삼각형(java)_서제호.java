08b605c60371aa55ae5ac61c4a1e90642e259425
seo.je.ho <sjho714@naver.com>
2024-04-29 13:20:40 +0900

    04-29품

---
+++ algo/WEEK04(dp)/정수삼각형(java)_서제호.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class 정수삼각형 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // 첫 번째 입력을 받음
+        sc.nextLine(); // 개행 문자 소비
+
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String line = sc.nextLine();
+            String[] numbers = line.split("\\s+"); // 공백으로 분리
+            ArrayList<Integer> row = new ArrayList<>();
+
+            for (String number : numbers) {
+                row.add(Integer.parseInt(number));
+            }
+            graph.add(row);
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < graph.get(i).size(); j++) {
+                if (j == 0) {
+                    graph.get(i).set(j, graph.get(i).get(j) + graph.get(i - 1).get(j));
+                } else if (j == (graph.get(i).size() - 1)) {
+                    graph.get(i).set(j, graph.get(i).get(j) + graph.get(i - 1).get(j - 1));
+                } else {
+                    graph.get(i).set(j, graph.get(i).get(j) + Math.max(graph.get(i - 1).get(j - 1), graph.get(i - 1).get(j)));
+                }
+            }
+        }
+
+        System.out.println(Collections.max(graph.get(n - 1)));
+        sc.close();
+    }
+}
+
