5ff763c3fe0c3579c36e4320a276cbaf71f7b63f
sdc05103 <heeni1004@gmail.com>
2024-05-05 19:39:50 +0900

    n과m(2)_chaeyoon추가

---
+++ algo/WEEK05(BackTraking)/N과 M시리즈/n과m(2)_chaeyoon.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+
+def dfs(start):
+    if len(arr)==M:
+        print(' '.join(map(str,arr)))
+        return
+    
+    for i in range(start,N+1):
+        if i not in arr:
+            arr.append(i)
+            dfs(i+1)
+            arr.pop()
+
+
+N,M=map(int,input().split())
+arr = []
+
+dfs(1)
