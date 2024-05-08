ccc68ea5ec5a5c9e0fd1643c6259cb29d5cd4214
KyungMinKang <74167204+YaRkyungmin@users.noreply.github.com>
2024-05-08 17:34:34 +0900

    n과m(1)_kyungmin.py

---
+++ algo/WEEK05(BackTraking)/N과 M시리즈/n과m_kyungmin/n과m(1)_kyungmin.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().rstrip().split())
+arr = [0 for _ in range(M)]
+visit = [False for _ in range(N + 1)]
+
+def permutatation(k):
+    global N, M
+    if k == M:
+        print(*arr)
+        return
+    for i in range(1, N + 1):
+        if not visit[i]:
+            arr[k] = i
+            visit[i] = True
+            permutatation(k + 1)
+            visit[i] = False
+
+permutatation(0)
