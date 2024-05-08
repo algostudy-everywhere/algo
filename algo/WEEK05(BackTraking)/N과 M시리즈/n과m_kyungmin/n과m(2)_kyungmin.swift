0eaed31a98a632e88017717c9c19342ea964bb5c
KyungMinKang <74167204+YaRkyungmin@users.noreply.github.com>
2024-05-08 17:36:21 +0900

    n과m(2)_kyungmin.swift

---
+++ algo/WEEK05(BackTraking)/N과 M시리즈/n과m_kyungmin/n과m(2)_kyungmin.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+func N과M2() {
+    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+    let N = input[0]
+    let M = input[1]
+    let permutation = Array(1...N)
+    
+    func findPermutation(k: Int, s: Int, arr: [Int]) {
+        if k == M {
+            print(arr.map { String($0) }.joined(separator: " "))
+            return
+        }
+
+        for i in s..<N {
+            var newArr = arr
+            newArr.append(permutation[i])
+            findPermutation(k: k + 1, s: i + 1, arr: newArr)
+        }
+    }
+    
+    findPermutation(k: 0, s: 0, arr: [])
+}
